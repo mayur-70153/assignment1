dc751711dac3cb7ca39f7e506c00122d5e6537f2
mayur-70153 <2021mt70153@wilp.bits-pilani.ac.in>
2023-03-06 16:31:43 +0530

    first commit

---
+++ src/devops/directory1.java
@@ -0,0 +1,13 @@
+package devops;
+
+public class directory1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a=1;
+		 int b=29;
+		 
+		 System.out.println(a+b);
+	}
+
+}
---
+++ src/devops2/directory2.java
@@ -0,0 +1,10 @@
+package devops2;
+
+public class directory2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
