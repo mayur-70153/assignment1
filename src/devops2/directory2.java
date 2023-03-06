285c3b26612ea15f0a5332776fc00dc8902a916f
mayur-70153 <2021mt70153@wilp.bits-pilani.ac.in>
2023-03-06 17:12:10 +0530

    Changes to source file and updating to github

--- src/devops2/directory2.java
+++ src/devops2/directory2.java
@@ -4,7 +4,12 @@ public class directory2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+			
+			int a=10;
+			int b=20;
+			
+			System.out.println(a*b);
+			System.out.println(a/b);
 	}
 
 }
